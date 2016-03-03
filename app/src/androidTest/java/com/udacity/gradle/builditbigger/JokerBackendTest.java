39d5d6dd4ff0acec494163fcd5c8b7bfcd9c8bd7
Martin Melcher <martin@inclumedia.at>
2016-03-03 19:52:01 +0100

    Flavours added

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/JokerBackendTest.java
@@ -0,0 +1,20 @@
+package com.udacity.gradle.builditbigger;
+
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by Martin Melcher on 03/03/16.
+ */
+public class JokerBackendTest extends AndroidTestCase {
+
+    public void testJokeIsFunny() {
+        new EndpointAsyncTask() {
+
+            @Override
+            protected void onPostExecute(String joke) {
+                assertTrue(joke != null && joke.length() > 0);
+            }
+        }.execute(getContext());
+    }
+}
